b4834a78b7c5a6d54bbc86053316b0e05f2d533d
Charles Kumar <gamescharlie79@gmail.com>
2025-11-11 15:27:37 +0530

    `change map location to tamilnadu

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([20.59, 78.96], 4);
+var map = L.map('map').setView([12.59, 78.96], 6);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([20.12, 78.04]).addTo(map)
+L.marker([12.12, 78.04]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
