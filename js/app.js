4557824187a57ff5f131fd1d638068ddc674fe5d
Charles Kumar <gamescharlie79@gmail.com>
2025-11-12 10:05:55 +0530

    update: include geoJSON from raw github url

--- js/app.js
+++ js/app.js
@@ -4,6 +4,16 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([20.12, 78.04]).addTo(map)
-    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-    .openPopup();
+// fetch('./INDIA_STATES.geojson')
+fetch('https://raw.githubusercontent.com/datta07/INDIAN-SHAPEFILES/refs/heads/master/INDIA/INDIA_STATES.geojson')
+            .then(response => response.json()) // Parse the response into a JSON object
+            .then(data => {
+                // 4. Add the GeoJSON data to the map once loaded
+                L.geoJSON(data, { }).addTo(map);
+                // Optional: Fit the map bounds to the new GeoJSON layer
+                // map.fitBounds(L.geoJSON(data).getBounds());
+            })
+            .catch(error => {
+                // Handle any errors during the fetch operation
+                console.log(`Error fetching GeoJSON: ${error}`);
+            });
